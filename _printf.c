21538c3480d59c528c7018d886e9eb175599ed27
hakeem-hassan <hlanre@ymail.com>
2023-12-04 18:29:20 -0500

    second commit

---
+++ _printf.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/*
+ * _printf- print a x to a standard output.
+ * @format: A place holder for the text.
+ *
+ * return  always 0 on success.
+ * on error -1 is return.
+ */
+int _printf(const char *format, ...)
+{
+}
