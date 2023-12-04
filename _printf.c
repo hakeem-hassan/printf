9132f0685a948a97a39860ebad27da3817527555
yfact <yfact247@gmail.com>
2023-12-04 15:45:51 -0800

    add printf file

--- _printf.c
+++ _printf.c
@@ -1,11 +1,21 @@
 #include "main.h"
+
 /*
- * _printf- print a x to a standard output.
+ * _printf - prints a x to a standard output.
  * @format: A place holder for the text.
  *
+ * Description: printf helps to write to stdout,
  * return  always 0 on success.
- * on error -1 is return.
+ * on error -1 is returned.
  */
+
 int _printf(const char *format, ...)
 {
+	int count = 0;
+	va_list arguments;
+
+	if (format == NULL)
+	{
+		return (-1);
+	}
 }
